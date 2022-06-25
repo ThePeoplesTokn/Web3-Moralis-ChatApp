b1fd185a58c2aec5a7ef64899fe30a759489d33a
Shaik Rehan <83860384+ShaikRehan123@users.noreply.github.com>
2022-06-25 07:08:37 +0530

    Update Login.jsx

--- components/Login.jsx
+++ components/Login.jsx
@@ -9,14 +9,14 @@ const Login = () => {
         <title>Login to Continue</title>
       </Head>
       <div className="absolute z-50 flex flex-col items-center justify-center w-full space-y-4 h-4/6">
-        <Image
-          src="https://links.papareact.com/3pi"
-          height={200}
-          width={200}
-          className="object-cover rounded-full"
-          loading="lazy"
-          draggable={false}
-        />
+//         <Image
+//           src="https://links.papareact.com/3pi"
+//           height={200}
+//           width={200}
+//           className="object-cover rounded-full"
+//           loading="lazy"
+//           draggable={false}
+//         />
         <button
           className="p-3 font-bold text-white bg-yellow-500 rounded-lg animate-bounce"
           onClick={() => authenticate({provider : 'metamask'})}
